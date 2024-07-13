cdf52a8cb40465b1ac6f0f07b9a08cec1bdf32f9
sinianluoye <372900338@qq.com>
2024-07-13 20:09:46 +0800

    add test pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manim-algorithm',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         "manim>=0.18.0"
